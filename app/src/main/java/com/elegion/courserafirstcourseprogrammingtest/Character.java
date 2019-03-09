adb87842b12b15aca5bc34d5cc9c74e24865278d
kevinche75 <47882198+kevinche75@users.noreply.github.com>
2019-03-09 17:48:26 +0300

    Initial commit

---
+++ app/src/main/java/com/elegion/courserafirstcourseprogrammingtest/Character.java
@@ -0,0 +1,194 @@
+package com.elegion.courserafirstcourseprogrammingtest;
+
+import java.io.Serializable;
+import java.util.Map;
+
+/**
+ * @author Azret Magometov
+ */
+
+public class Character implements Serializable {
+
+    // TODO - Почему это захардкожено?
+    private static final int ATTACK_SEED = 4;
+    private static final int PHYSICAL_DEFENSE_SEED = 4;
+    private static final int MAGIC_DEFENSE_SEED = 4;
+    private static final int DODGE_SEED = 4;
+    private static final int CRITICAL_ATTACK_SEED = 4;
+    private static final int HP_SEED = 4;
+    private static final int MANA_SEED = 4;
+
+
+    private String mName;
+    private CharacterCreator.Race mRace;
+    private CharacterCreator.Specialization mSpecialization;
+    private Map<String, Integer> mAttributesMap;
+
+    private int mClassParameter;
+    private float mAttack;
+    private float mPhysicalDefense;
+    private float mMagicDefense;
+    private float mDodge;
+    private float mCriticalHitChance;
+    private int mHitPoints;
+    private int mManaPoints;
+
+    private Map<String, Boolean> mPerks;
+
+    public void setName(String name) {
+        mName = name;
+    }
+
+    public void setRace(CharacterCreator.Race race) {
+        mRace = race;
+    }
+
+    public void setSpecialization(CharacterCreator.Specialization personClass) {
+        mSpecialization = personClass;
+    }
+
+    public String getAttack() {
+        return String.valueOf(mAttack);
+    }
+
+    public String getPhysicalDefense() {
+        return String.valueOf(mPhysicalDefense);
+    }
+
+    public String getMagicDefense() {
+        return String.valueOf(mMagicDefense);
+    }
+
+    public String getHitPoints() {
+        return String.valueOf(mHitPoints);
+    }
+
+    public String getManaPoints() {
+        return String.valueOf(mManaPoints);
+    }
+
+    public String getDodge() {
+        return String.valueOf(mDodge);
+    }
+
+    public String getCriticalHitChance() {
+        return String.valueOf(mCriticalHitChance);
+    }
+
+    public void calculateParameters() {
+        switch (mSpecialization) {
+            case WARRIOR:
+                mClassParameter = mAttributesMap.get(CharacterCreator.Attribute.STRENGTH.name());
+                mManaPoints = 0;
+                break;
+            case MAGE:
+                mClassParameter = mAttributesMap.get(CharacterCreator.Attribute.INTELLECT.name());
+                mManaPoints = mClassParameter * MANA_SEED;
+                break;
+            case ARCHER:
+                mClassParameter = mAttributesMap.get(CharacterCreator.Attribute.AGILITY.name());
+                mManaPoints = 0;
+                break;
+        }
+        mAttack = mClassParameter * ATTACK_SEED;
+        mMagicDefense = mAttributesMap.get(CharacterCreator.Attribute.INTELLECT.name()) * MAGIC_DEFENSE_SEED;
+        mPhysicalDefense = mAttributesMap.get(CharacterCreator.Attribute.STRENGTH.name()) * PHYSICAL_DEFENSE_SEED;
+        mDodge = mAttributesMap.get(CharacterCreator.Attribute.AGILITY.name()) * DODGE_SEED;
+        mCriticalHitChance = mAttributesMap.get(CharacterCreator.Attribute.LUCK.name()) * CRITICAL_ATTACK_SEED + mClassParameter;
+        mHitPoints = mAttributesMap.get(CharacterCreator.Attribute.STAMINA.name()) * HP_SEED + mClassParameter;
+
+        //Boolean isBerserk = mPerks.get(CharacterCreator.Perk.BERSERK.name());
+//        if (isBerserk != null && isBerserk) {
+//            mAttack *= 1.1;
+//            mPhysicalDefense *= 0.85;
+//            mMagicDefense *= 0.85;
+//        }
+        String string = CharacterCreator.Perk.BERSERK.name();
+        boolean isBerserk = false;
+        if(mPerks.containsKey("Berserk") && mPerks.get("Berserk")!=null){
+            isBerserk = mPerks.get("Berserk");
+        }
+        if (isBerserk) {
+            mAttack *= 1.1;
+            mPhysicalDefense *= 0.85;
+            mMagicDefense *= 0.85;
+        }
+
+        boolean isCalm = false;
+        if(mPerks.containsKey("Calm")&& mPerks.get("Calm")!=null)
+        isCalm = mPerks.get("Calm");
+        if (isCalm) {
+            mAttack *= 0.85;
+            mPhysicalDefense *= 1.1;
+            mMagicDefense *= 1.1;
+        }
+
+        boolean isLightWeight = false;
+        if(mPerks.containsKey("Lightweight")&& mPerks.get("Lightweight")!=null)
+            isLightWeight = mPerks.get("Lightweight");
+        if (isLightWeight) {
+            mDodge *= 1.1;
+            mPhysicalDefense *= 0.85;
+        }
+
+        boolean isHeavyArmored = false;
+        if(mPerks.containsKey("Heavyarmored")&& mPerks.get("Heavyarmored")!=null)
+        isHeavyArmored = mPerks.get("Heavyarmored");
+        if (isHeavyArmored) {
+            mPhysicalDefense *= 1.1;
+            mDodge *= 0.85;
+        }
+
+        boolean isMeditations = false;
+        if(mPerks.containsKey("Meditations")&& mPerks.get("Meditations")!=null)
+        isMeditations = mPerks.get("Meditations");
+        if (isMeditations) {
+            mMagicDefense *= 1.1;
+            mCriticalHitChance *= 0.85;
+        }
+        boolean isObservant = false;
+        if(mPerks.containsKey("Observant")&& mPerks.get("Observant")!=null)
+        isObservant = mPerks.get("Observant");
+        if (isObservant) {
+            mCriticalHitChance *= 1.1;
+            mMagicDefense *= 0.85;
+        }
+
+    }
+
+    @Override
+    public String toString() {
+        return "Character{" +
+                "mRace=" + mRace +
+                ", mSpecialization=" + mSpecialization +
+                ", mAttack=" + mAttack +
+                ", mPhysicalDefense=" + mPhysicalDefense +
+                ", mMagicDefense=" + mMagicDefense +
+                ", mDodge=" + mDodge +
+                ", mCriticalHitChance=" + mCriticalHitChance +
+                ", mHitPoints=" + mHitPoints +
+                ", mManaPoints=" + mManaPoints +
+                '}';
+    }
+
+    public void setAttributes(Map<String, Integer> attributesMap) {
+        mAttributesMap = attributesMap;
+    }
+
+    public void setPerks(Map<String, Boolean> perks) {
+        mPerks = perks;
+    }
+
+
+    public String getName() {
+        return mName;
+    }
+
+    public String getRace() {
+        return mRace.name().substring(0, 1) + mRace.name().substring(1).toLowerCase();
+    }
+
+    public String getSpecialization() {
+        return mSpecialization.name().substring(0, 1) + mSpecialization.name().substring(1).toLowerCase();
+    }
+}
